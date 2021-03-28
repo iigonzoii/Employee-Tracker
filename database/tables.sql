f0b268fc90f1efb04e2e59181c8d8ccb820590ca
iigonzoii <a.gonzalez418@gmail.com>
2021-03-28 10:35:14 -0600

    added employee table

--- database/tables.sql
+++ database/tables.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS employees;
+DROP DATABASE IF EXISTS employee_tracker;
 
-CREATE DATABASE employees;
+CREATE DATABASE employees_tracker;
 
-USE employees;
+USE employee_tracker;
 
 -- here we create our Departmnent table
-CREATE TABLE department(
+CREATE TABLE department (
     -- here we require an id and set it to auto increment
     id INT NOT NULL auto_increment,
     -- make a row called name
@@ -13,8 +13,9 @@ CREATE TABLE department(
     -- set primary key to id
     PRIMARY KEY(id)
 );
+
 -- here we create a table for our employees roles
-CREATE TABLE role(
+CREATE TABLE role (
     -- here we require an id and set it to auto increment
     id INT NOT NULL auto_increment,
     -- make a row called title
@@ -31,7 +32,32 @@ CREATE TABLE role(
     ON DELETE CASCADE,
     -- set primary key to id
     PRIMARY KEY(id)
-)
+);
+
+-- here we create our employee table
+CREATE TABLE employee (
+-- here we require an id and set it to auto increment
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    -- make a row called first name
+    first_name VARCHAR(30),
+    -- make a row called last name
+    last_name VARCHAR(30),
+-- make a row called role id
+    role_id INT,
+    -- make  a row called manager id
+    manager_id INT,
+    -- here we establish our foreign id and the location it was defined and that we are refrencing
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    -- with cascade we update both parent and child tables at the same time
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    -- here we establish our foreign id and the location it was defined and that we are refrencing
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    -- with cascade we update both parent and child tables at the same time
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+
+);
 
 
 
